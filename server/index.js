2e3bf878f48db46458145b0732512c739bda598f
Urvish Patel <uvpatel2510@gmail.com>
2022-03-29 11:16:29 +0530

    Basic File Structure Completed

--- server/index.js
+++ server/index.js
@@ -3,4 +3,23 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
+import postRoutes from "./routes/posts.js";
+
 const app = express();
+
+app.use("/posts", postRoutes);
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+const CONNECTION_URL =
+  "mongodb+srv://PosTTalK:HnZTK6qhm6xwCpB@posttalk.r94vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const PORT = process.env.PORT || 1155;
+
+mongoose
+  .connect(CONNECTION_URL)
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on PORT http://localhost:${PORT}`))
+  )
+  .catch((error) => console.log(error.message));
