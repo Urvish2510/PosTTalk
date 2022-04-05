55e2b55696be6f6382de97664e5960df546c1861
Urvish Patel <uvpatel2510@gmail.com>
2022-04-05 14:36:21 +0530

    Redux setup completed, Fetching files from mongo using axios and show them on frontend in component completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import './App.css';
+import React, { useEffect } from "react";
+import { Container, AppBar, Typography, Grow, Grid } from "@material-ui/core";
+import { useDispatch } from "react-redux";
 
-function App() {
+import { getPosts } from "./actions/posts";
+import Posts from "../src/components/Posts/Posts";
+import Form from "../src/components/Form/Form";
+import memories from "../src/images/memories.png";
+import useStyles from "../src/styles";
+
+const App = () => {
+  const classes = useStyles();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [dispatch]);
+
   return (
-    <div className="App">
-        <h1>App</h1>
-    </div>
+    <Container maxWidth="lg">
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography className={classes.heading} variant="h2" align="center">
+          CircuLator
+        </Typography>
+        <img
+          className={classes.image}
+          src={memories}
+          alt="Circulator"
+          height="60"
+        />
+      </AppBar>
+      <Grow in>
+        <Container>
+          <Grid
+            container
+            justify="space-between"
+            alignItems="stretch"
+            spacing={3}
+          >
+            <Grid item xs={12} sm={7}>
+              <Posts />
+            </Grid>
+            <Grid item xs={12} sm={4}>
+              <Form />
+            </Grid>
+          </Grid>
+        </Container>
+      </Grow>
+    </Container>
   );
-}
+};
 
 export default App;
--- server/index.js
+++ server/index.js
@@ -6,12 +6,11 @@ import cors from "cors";
 import postRoutes from "./routes/posts.js";
 
 const app = express();
-
-app.use("/posts", postRoutes);
-
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+app.use("/posts", postRoutes);
 
 const CONNECTION_URL =
   "mongodb+srv://PosTTalK:HnZTK6qhm6xwCpB@posttalk.r94vg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -20,6 +19,8 @@ const PORT = process.env.PORT || 1155;
 mongoose
   .connect(CONNECTION_URL)
   .then(() =>
-    app.listen(PORT, () => console.log(`Server running on PORT http://localhost:${PORT}`))
+    app.listen(PORT, () =>
+      console.log(`Server running on PORT http://localhost:1155`)
+    )
   )
   .catch((error) => console.log(error.message));
