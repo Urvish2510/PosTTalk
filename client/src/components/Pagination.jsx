5b7145b5946542f7f53835e64cf4e718cb54e289
Urvish Patel <uvpatel2510@gmail.com>
2022-04-18 17:37:43 +0530

    Started with new paginate component

---
+++ client/src/components/Pagination.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Pagination, PaginationItem } from "@material-ui/lab";
+
+import useStyles from "./styles";
+
+const Paginate = () => {
+    const classes = useStyles();
+
+    return(
+        
+    )
+}
