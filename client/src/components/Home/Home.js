c16b5a3388c88bc46b3c6c943b57ee9664fc68d0
Urvish Patel <uvpatel2510@gmail.com>
2022-06-08 16:47:31 +0530

    Scarch functionality start

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -17,6 +17,8 @@ import Pagination from "../Pagination";
 import Posts from "../Posts/Posts";
 import Form from "../Form/Form";
 
+import useStyles from "./styles";
+
 function useQuery() {
   return new URLSearchParams(useLocation().search);
 }
@@ -27,24 +29,63 @@ const Home = () => {
   const query = useQuery();
   const navigate = useNavigate();
   const page = query.get("page");
+  const searchQuery = query.get("searchQuery");
+  const classes = useStyles();
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
 
   useEffect(() => {
     dispatch(getPosts());
   }, [currentId, dispatch]);
 
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      //search post
+    }
+  };
+
+  const handleAdd = (tag) => setTags([...tags, tag]);
+
+  const handleDelete = (tagToDelete) =>
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+
   return (
     <Grow in>
-      <Container>
+      <Container maxWidth="xl">
         <Grid
           container
           justifyContent="space-between"
           alignItems="stretch"
           spacing={3}
+          className={classes.gridContainer}
         >
-          <Grid item xs={12} sm={7}>
+          <Grid item xs={12} sm={6} md={9}>
             <Posts setCurrentId={setCurrentId} />
           </Grid>
-          <Grid item xs={12} sm={4}>
+          <Grid item xs={12} sm={4} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Posts"
+                fullWidth
+                onKeyPress={handleKeyPress}
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <ChipInput
+                style={{ margin: "10px 0" }}
+                value={tags}
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+                label="Search Tags"
+                variant="outlined"
+              />
+            </AppBar>
             <Form currentId={currentId} setCurrentId={setCurrentId} />
             <Paper elevation={6}>
               <Pagination />
